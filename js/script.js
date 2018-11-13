0997147d036f6962bb3ad22c4c71292c19a54adf
Leon Lei <leonlei35t@gmail.com>
2018-11-12 23:51:17 -0500

    Added: language constructs to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-var x = 'Hello Foobar'
+// ****** String Concatenation
+var string = "Hello";
+string += " World";
+console.log(string + "!");
+
+// ****** Math operators
+console.log((5 + 4) / 3);
+console.log(undefined / 5)
+function test1 (a) {
+    console.log( a / 5);
+}
+test1();
+test1(5);
+
+// ****** Equality and Type Coercion
+var x = 4, y = 4;
+if (x == y) {
+    console.log("x=4 is equal to y=4");
+}
+
+x = "4";
+if (x == y) {
+    console.log("x='4' is equal to y=4");
+}
+
+// ****** Strict equality
+if (x === y) {
+    console.log("Strict: x='4' is equal to y=4");
+}
+else {
+    console.log("Strict: x='4' is NOT equal to y=4")
+}
+
+// ****** If statement (all false)
+if ( false || null || 
+    undefined || "" || 0 || NaN) {
+        console.log("This line won't ever execute");
+    }
+    else {
+        console.log("All false");
+    }
+    
+// ****** If statement (all true)
+if (true && "hello" && 1 && -1 && "false") {
+    console.log("All true");
+}
